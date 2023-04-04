8d9789cf97689d19b8516ac5fe63c0fd66eba30d
ValdineiJunior2004 <129808151+ValdineiJunior2004@users.noreply.github.com>
2023-04-03 22:09:54 -0300

    Add files via upload

---
+++ atv1.py
@@ -0,0 +1,14 @@
+nome = "valdinei"
+sobrenome = "pereira de lima junior"
+idade = 19
+altura = 1.75
+peso = 70.5
+maior_de_idade = True
+
+# Exibe as informações na tela
+print("Nome:", nome)
+print("Sobrenome:", sobrenome)
+print("Idade:", idade)
+print("Altura:", altura)
+print("Peso:", peso)
+print("Maior de idade:", maior_de_idade)
